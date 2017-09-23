81aba89131579af132eb57858077d63c59ad84ef
zigext <zigext@gmail.com>
2017-09-23 16:03:31 +0700

    Add sound buttons

--- HPHRp1/app/components/ExerciseAdvicesSwiper.js
+++ HPHRp1/app/components/ExerciseAdvicesSwiper.js
@@ -8,7 +8,7 @@ import Swiper from 'react-native-swiper'
 import styles from '../styles/index'
 import Sound from 'react-native-sound'
 
-export default class FoodAdvicesSwiper extends React.Component {
+export default class ExerciseAdvicesSwiper extends React.Component {
     constructor() {
         super()
 
@@ -59,27 +59,32 @@ export default class FoodAdvicesSwiper extends React.Component {
                 <View style={_styles.slide}>
                     <Image source={require('../../assets/images/food1.png')} style={_styles.image}></Image>
                     <Icon reverse name='controller-play' type='entypo' color='#f49842' onPress={() => this.playSound('slide1')} />
-                    <Text style={styles.text}>ควรหลีกเลี่ยงอาหารที่ทำให้คลอเลสเตอรอลในเลือดสูง เช่น ไข่แดง เครื่องในสัตว์ อาหารทะเล</Text>
+                    <Text style={styles.text}>รูปแบบการออกกําลังกาย ใช้การเดินและการออกกําลังกายแบบไอโซโทนิคเป็นหลัก คือการออกกําลังกายชนิดที่มีการยืด/หดตัวของกล้ามเนื้อ และอวัยวะมีการเคลื่อนไหวขณะออกกําลัง</Text>
                 </View>
                 <View style={_styles.slide}>
                     <Image source={require('../../assets/images/food2.jpg')} style={_styles.image}></Image>
                     <Icon reverse name='controller-play' type='entypo' color='#f49842' onPress={() => this.playSound('slide2')} />
-                    <Text style={styles.text}>ควรงดชา กาแฟ น้ำอัดลม เครื่องดื่มที่มีแอลกอฮอล์ และควรงดสูบบุหรี่</Text>
+                    <Text style={styles.text}>ผู้ป่วยโรคกล้ามเนื้อหัวใจตายเฉียบพลันควรออกกําลังกายโดยชีพจรขณะออกกําลังกายมากกว่าชีพจรขณะพักไม่เกิน 20 ครั้งต่อนาที ส่วนผู้ป่วยหลังผ่าตัดหัวใจหรือการรักษาโดยการขยายหลอดเลือดหัวใจด้วยบอลลูน ควรออกกําลังกายโดยชีพจรขณะออกกําลังกายมากกว่าชีพจรขณะพักไม่เกิน 30 ครั้งต่อนาที</Text>
                 </View>
                 <View style={_styles.slide}>
                     <Image source={require('../../assets/images/food3.jpg')} style={_styles.image}></Image>
                     <Icon reverse name='controller-play' type='entypo' color='#f49842' onPress={() => this.playSound('slide3')} />
-                    <Text style={styles.text}>หลีกเลี่ยงอาหารที่ทำให้น้ำตาลในเลือดสูง เช่น ข้าวเหนียวทุเรียน ขนมหวาน มะม่วง ลำไย</Text>
+                    <Text style={styles.text}>ระยะเวลา เริ่มจาก 5-10 นาที และเพิ่มเป็น 20-30 นาที ให้รู้สึกเหนื่อยเล็กน้อยหรือปานกลาง (RPE 9-12)</Text>
                 </View>
                 <View style={_styles.slide}>
                     <Image source={require('../../assets/images/food4.jpg')} style={_styles.image}></Image>
                     <Icon reverse name='controller-play' type='entypo' color='#f49842' onPress={() => this.playSound('slide4')} />
-                    <Text style={styles.text}>ถ้าต้องใช้น้ำมันปรุงอาหารควรเลือกใช้น้ำมันพืช แทนเนยหรือน้ำมันจากสัตว์ หรือใช้การลวก ต้ม นึ่ง และอบแทนการทอด</Text>
+                    <Text style={styles.text}>ความถี่ วันละ 2 ครั้ง ขึ้นกับสภาพร่างกาย</Text>
                 </View>
                 <View style={_styles.slide}>
                     <Image source={require('../../assets/images/food5.jpg')} style={_styles.image}></Image>
                     <Icon reverse name='controller-play' type='entypo' color='#f49842' onPress={() => this.playSound('slide5')} />
-                    <Text style={styles.text}>ควรรับประทานผักทุกชนิด ผลไม้ที่ไม่มีรสหวานจัด เช่น มะละกอ แอปเปิ้ล ฝรั่ง</Text>
+                    <Text style={styles.text}>ค่อยๆเพิ่มความหนักในการออกกําลังกาย เริ่มจากเคลื่อนไหวข้อจากข้อเล็กๆ เช่น ข้อมือ ข้อเท้า ไปยังข้อที่ใหญ่ เช่น คอ ไหล่ กระทําอย่างต่อเนื่อง ไม่เกร็งค้าง และไม่กลั้นหายใจขณะทํา อาจมีคนช่วยทําหลังจากนั้นก็ทําด้วยตนเอง ค่อยๆเปลี่ยนท่าจากนอน ไปนั่ง และยืน</Text>
+                </View>
+                <View style={_styles.slide}>
+                    <Image source={require('../../assets/images/food5.jpg')} style={_styles.image}></Image>
+                    <Icon reverse name='controller-play' type='entypo' color='#f49842' onPress={() => this.playSound('slide5')} />
+                    <Text style={styles.text}>การเดิน เป็นการออกกําลังกายทําได้ง่ายและช่วยเสริมให้ผู้ป่วยประกอบกิจวัตรประจําวันได้ดียิ่งขึ้น โดยพยายามเดินให้ระยะทางไกลขึ้นในครั้งต่อไป  และสามารถเดินได้เร็วขึ้นถ้าไม่มีอาการไม่พึงประสงค์ เช่น มึนงง เซ ซีด เขียว หายใจลําบาก คลื่นไส้ เจ็บหน้าอก</Text>
                 </View>
             </Swiper>
         )
