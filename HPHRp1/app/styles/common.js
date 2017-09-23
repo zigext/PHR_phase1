58854f6eaeff45429746377b7b7797bb298d3269
zigext <zigext@gmail.com>
2017-09-23 16:05:14 +0700

    Add accent color

--- HPHRp1/app/styles/common.js
+++ HPHRp1/app/styles/common.js
@@ -2,5 +2,6 @@ export default common = {
     primaryColor : '#29B6F6',
     primaryColorDark: '#039BE5',
     grey: '#474045',
-    backgroundWhite: '#f7f1e6'
+    backgroundWhite: '#f7f1e6',
+    accentColor: '#f49842'
 }
