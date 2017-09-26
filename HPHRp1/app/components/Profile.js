b17867a4f1488b92e2b6da138bbc3e5affe8f1f8
zigext <zigext@gmail.com>
2017-09-26 13:14:11 +0700

    Edit layout

--- HPHRp1/app/components/Profile.js
+++ HPHRp1/app/components/Profile.js
@@ -21,33 +21,34 @@ export default class Profile extends React.Component {
     render() {
         console.log("str ", this.changeArrayToString(this.props.profile.allergic_food))
         return (
-            <ScrollView>
-            <View style={_styles.container}>
-                <View style={_styles.pictureContainer}>
-                    <Text>Profile picture</Text>
+            
+                <View style={_styles.container}>
+                    <View style={_styles.pictureContainer}>
+                        {/*user can upload picture and edit profile*/}
+                        <Image source={require('../../assets/images/user.png')} style={_styles.image}></Image>
+                    </View>
+                    <ScrollView>
+                    <View style={_styles.contentContainer}>
+                        <Text style={[styles.text, {fontWeight: 'bold', marginBottom: 25, marginTop: 20}]}>ประวัติส่วนตัว</Text>
+                        <Text style={_styles.text}>ชื่อ : {this.props.profile.firstname} {this.props.profile.lastname}</Text>
+                        <Text style={_styles.text}>รหัสผู้ป่วย : {this.props.profile.patient_code}</Text>
+                        <Text style={_styles.text}>เพศ : {this.props.profile.gender}</Text>
+                        <Text style={_styles.text}>ส่วนสูง : {this.props.profile.height} เซนติเมตร</Text>
+                        <Text style={_styles.text}>น้ำหนัก : {this.props.profile.weight} กิโลกรัม</Text>
+                        <Text style={_styles.text}>หมู่เลือด : {this.props.profile.blood_type}</Text>
+                        <Text style={_styles.text}>วันเกิด : {this.props.profile.birthday}</Text>
+                        <Text style={_styles.text}>ที่อยู่ : {this.props.profile.address}</Text>
+                        <Text style={_styles.text}>เบอร์โทรศัพท์ : {this.props.profile.phone}</Text>
+                        <Text style={_styles.text}>เบอร์โทรศัพท์ญาติ : {this.props.profile.kin_phone}</Text>
+                        <Text style={_styles.text}>อาหารที่แพ้ : {this.props.profile.allergic_food ? this.changeArrayToString(this.props.profile.allergic_food) : '-'}</Text>
+                        <Text style={_styles.text}>ยาที่แพ้ : {this.props.profile.allergic_medicine ? this.changeArrayToString(this.props.profile.allergic_medicine) : '-'}</Text>
+                        <Text style={_styles.text}>ยาปัจจุบัน : {this.props.profile.current_medicine ? this.changeArrayToString(this.props.profile.current_medicine) : '-'}</Text>
+                        <Text style={_styles.text}>ภาวะทางการแพทย์ : {this.props.profile.medical_condition ? this.changeArrayToString(this.props.profile.medical_condition) : '-'}</Text>
+                    </View>
+                    </ScrollView>
+
                 </View>
-                
-                <View style={_styles.contentContainer}>
-                            <Text style={styles.text}>ประวัติส่วนตัว</Text>
-                            <Text style={_styles.text}>ชื่อ : {this.props.profile.firstname} {this.props.profile.lastname}</Text>
-                            <Text style={_styles.text}>รหัสผู้ป่วย : {this.props.profile.patient_code}</Text>
-                            <Text style={_styles.text}>เพศ : {this.props.profile.gender}</Text>
-                            <Text style={_styles.text}>ส่วนสูง : {this.props.profile.height} เซนติเมตร</Text>
-                            <Text style={_styles.text}>น้ำหนัก : {this.props.profile.weight} กิโลกรัม</Text>
-                            <Text style={_styles.text}>หมู่เลือด : {this.props.profile.blood_type}</Text>
-                            <Text style={_styles.text}>วันเกิด : {this.props.profile.birthday}</Text>
-                            <Text style={_styles.text}>ที่อยู่ : {this.props.profile.address}</Text>
-                            <Text style={_styles.text}>เบอร์โทรศัพท์ : {this.props.profile.phone}</Text>
-                            <Text style={_styles.text}>เบอร์โทรศัพท์ญาติ : {this.props.profile.kin_phone}</Text>
-                            <Text style={_styles.text}>อาหารที่แพ้ : {this.props.profile.allergic_food? this.changeArrayToString(this.props.profile.allergic_food): '-'}</Text>
-                            <Text style={_styles.text}>ยาที่แพ้ : {this.props.profile.allergic_medicine? this.changeArrayToString(this.props.profile.allergic_medicine): '-'}</Text>
-                            <Text style={_styles.text}>ยาปัจจุบัน : {this.props.profile.current_medicine? this.changeArrayToString(this.props.profile.current_medicine): '-'}</Text>
-                            <Text style={_styles.text}>ภาวะทางการแพทย์ : {this.props.profile.medical_condition? this.changeArrayToString(this.props.profile.medical_condition): '-'}</Text>
-                </View>
-                
-                
-            </View>
-            </ScrollView>
+            
         )
     }
 }
@@ -55,32 +56,34 @@ export default class Profile extends React.Component {
 const _styles = StyleSheet.create({
     wrapper: {
     },
-    slide: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        padding: 20,
-        paddingHorizontal: 50,
-        margin: 10
-    },
     text: {
         color: '#474045',
-        fontSize: 25
+        fontSize: 25,
+        marginBottom: 10
     },
     image: {
-        resizeMode: 'center',
+        height: 200,
+        borderRadius: 100,
+        width: 200,
         margin: 10,
-        flex: 1
+        alignSelf: 'center'
     },
     container: {
-        flexDirection: 'column',
+        flexDirection: 'row',
         flex: 1
     },
     contentContainer: {
-        alignItems: 'stretch'
+        alignItems: 'stretch',
+        borderColor: '#f49842',
+        borderWidth: 3,
+        borderRadius: 10,
+        padding: 15,
+        margin: 20,
+        flex: 0.7
     },
     pictureContainer: {
         alignItems: 'stretch',
-        backgroundColor: '#0ccbf2'
+        flex: 0.3,
+        opacity: 60
     }
 })
