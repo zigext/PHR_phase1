ab32106a235c31f7e1574cef61819a305bc08236
zigext <zigext@gmail.com>
2017-09-26 13:18:31 +0700

    Container show 2 actvity advices buttons

---
+++ HPHRp1/app/containers/ActivityAdvices.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { Actions } from 'react-native-router-flux'
+import { connect } from 'react-redux'
+import Activity1 from '../components/Activity1AdvicesButton'
+import Activity2 from '../components/Activity2AdvicesButton'
+import Orientation from 'react-native-orientation'
+
+
+class FoodAdvices extends React.Component {
+    constructor() {
+        super()
+
+    }
+
+    componentDidMount() {
+        Orientation.lockToLandscape();
+    }
+
+    onActivity1Press = () => {
+        Actions.tab_advices_activity_1()
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+               <Activity1 onActivity1Press={this.onActivity1Press}/>
+               <Activity2 />
+            </View>
+        )
+    }
+}
+
+const mapStateToProps = (state) => {
+    console.log("mapStateToProps", state)
+    return state
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        // dispatchLogIn: (email, uid) => dispatch(logIn(email, uid))
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(FoodAdvices)
+
+var styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'row',
+        backgroundColor: '#FFFDF9',
+        justifyContent: 'center'
+    }
+})
