f4ceaf674e11fffff0a4ca6d3b34599d50a27ada
zigext <zigext@gmail.com>
2017-11-04 01:04:31 +0700

    Add new surgery by calling server's API

--- HPHRp1/app/containers/AddSurgery.js
+++ HPHRp1/app/containers/AddSurgery.js
@@ -5,6 +5,8 @@ import AddSurgeryForm from '../components/AddSurgeryForm'
 import { Actions } from 'react-native-router-flux'
 import { connect } from 'react-redux'
 import { addNewSurgery } from '../actions/userAction'
+import ApiUtils from '../components/ApiUtils'
+import { SERVER_IP, SURGERY } from '../config/Const'
 
 // *******ERROR*******
 // child keys in firebase have correct time and date
@@ -13,17 +15,48 @@ import { addNewSurgery } from '../actions/userAction'
 class AddSurgery extends React.Component {
 
     onAddSurgeryPress = (newSurgery, date, time, doctor, hospital, type, note, callback) => {
-       
-        firebase.database().ref(`surgery/${this.props.default.user.uid}_${hospital}_${date}_${time}`).set(newSurgery).
-            then((data) => {
-                console.log("add new surgery to database success")
+        this.addSurgery(newSurgery, hospital, date, time, callback)
+        // firebase.database().ref(`surgery/${this.props.default.user.uid}_${hospital}_${date}_${time}`).set(newSurgery).
+        //     then((data) => {
+        //         console.log("add new surgery to database success")
+        //         this.props.dispatchAddNewSurgery()
+        //         callback(null)
+        //         Actions.surgery()
+        //     }).
+        //     catch((err) => {
+        //         console.log("add new surgery to database failed")
+        //         callback(err)
+        //     })
+    }
+
+    addSurgery = async (newSurgery, hospital, date, time, callback) => {
+        console.log("test ", hospital, date, time, newSurgery)
+        const path = `${SERVER_IP}${SURGERY}`
+        await fetch(path, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                userid: '1416382941765846', //this.props.default.user.uid
+                hospitalid: hospital, 
+                date: date,
+                time: time,
+                information: newSurgery
+            })
+        })
+            .then(ApiUtils.checkStatus)
+            .then(responseData => {
+                console.log("Add surgery success")
                 this.props.dispatchAddNewSurgery()
                 callback(null)
                 Actions.surgery()
-            }).
-            catch((err) => {
-                console.log("add new surgery to database failed")
-                callback(err)
+
+            })
+            .catch(error => {
+                console.log("Add surgery failed = ", error)
+                callback(error)
             })
     }
 
