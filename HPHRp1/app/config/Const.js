9fec4ce798ea559ea571c437aba41dea669bfb5c
zigext <zigext@gmail.com>
2017-11-03 23:13:00 +0700

    Constants for calling server's APIs

---
+++ HPHRp1/app/config/Const.js
@@ -0,0 +1,7 @@
+const SERVER_IP = 'http://192.168.1.7:5000/'
+const PROFILE = 'profile/info'
+const SURGERY = 'surgery/info'
+const ACTIVITY_RESULT_1 = 'activity_result/1'
+const ACTIVITY_RESULT_1_ALL = 'activity_result/1/all'
+
+export {SERVER_IP, PROFILE, SURGERY, ACTIVITY_RESULT_1, ACTIVITY_RESULT_1_ALL}
