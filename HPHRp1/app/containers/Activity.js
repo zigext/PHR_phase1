e82f472a2385f5549ac67566938a6cee3e4f7272
zigext <zigext@gmail.com>
2017-12-27 22:58:14 +0700

    Split doingActivity to another container

--- HPHRp1/app/containers/Activity.js
+++ HPHRp1/app/containers/Activity.js
@@ -1,26 +1,23 @@
 import React from 'react'
 import { StyleSheet, Text, View, Alert } from 'react-native'
 import { Icon, Button } from 'react-native-elements'
-import firebase from '../config/Firebase'
-import ActivityProgress from '../components/ActivityProgress'
-import Timer from '../components/Timer'
+import ApiUtils from '../components/ApiUtils'
 import PreActivity from './PreActivity'
-import PreActivityForm from '../components/PreActivityForm'
-import Instructions from '../components/Instructions'
-import Stepper from '../components/Stepper'
+import DoingActivity from './DoingActivity'
 import Borg from '../components/Borg'
 import { Actions } from 'react-native-router-flux'
 import { connect } from 'react-redux'
 import Orientation from 'react-native-orientation'
 import moment from 'moment'
+import { isEmpty } from 'lodash'
 
 class Activity extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            state: 'pre activity',
+            state: 'doing activity',
             level: 0,
-            progress: 0,
+            profile: {},
             timeStart: new Date(),
             timeStop: '',
             duration: '',
@@ -29,6 +26,26 @@ class Activity extends React.Component {
     }
     componentDidMount() {
         Orientation.lockToLandscape()
+        if(isEmpty(this.state.profile)) {
+            // this.fetchProfile()
+        }
+    }
+
+    fetchProfile = async () => {
+        const path = `${SERVER_IP}${PROFILE}?userid=1416382941765846&appid=PHRapp` //userid=${this.props.default.user.uid}&appid=${this.props.default.appId}  //userid=${this.props.UserReducer.user.uid}&appid=${this.props.UserReducer.appId}
+        await fetch(path)
+            .then(ApiUtils.checkStatus)
+            .then(response => response.json())
+            .then(responseData => {
+                this.profile = responseData.data.profile
+                this.setState({ profile: this.profile })
+                // this.props.dispatchProfile(this.state.profile)
+                console.log("Fetch profile success")
+                
+            })
+            .catch(error => {
+                console.log("Error in fetchProfile = ", error)
+            })
     }
 
     //calculate duration from local time (new Date())
@@ -96,97 +113,99 @@ class Activity extends React.Component {
 
     renderPreActivity = () => {
         return (
-          <PreActivity onPreActivityDone={this.onPreActivityDone} />
-        // <View style={styles.container}>
-        //     <Instructions />
-        //     <PreActivityForm onSubmitPreActivity={this.onSubmitPreActivity} />
-            
-        // </View>
+            <PreActivity onPreActivityDone={this.onPreActivityDone} firstname={this.state.profile.firstname} lastname={this.state.profile.lastname} patientCode={this.state.profile.patient_code}/>
+            // <View style={styles.container}>
+            //     <Instructions />
+            //     <PreActivityForm onSubmitPreActivity={this.onSubmitPreActivity} />
+
+            // </View>
         )
     }
 
     renderDoingActivity = () => {
-        let str = ''
-        switch (this.state.level) {
-            case 0: {
-                str = 'ระดับ 1 นั่งบนเตียง ออกกำลังกายบนเตียง'
-                break
-            }
-            case 1: {
-                str = 'ระดับ 2 มีผู้ช่วยเหลือผู้ป่วยมานั่งบนเก้าอี้ (ไม่ยืน)'
-                break
-            }
-            case 2: {
-                str = 'ระดับ 3 นั่งที่ขอบเตียง'
-                break
-            }
-            case 3: {
-                str = 'ระดับ 4 ยืนที่ข้างเตียง'
-                break
-            }
-            case 4: {
-                str = 'ระดับ 5 สามารถย้ายจากเตียงไปนั่งที่เก้าอี้ได้'
-                break
-            }
-            case 5: {
-                str = 'ระดับ 6 ก้าวขา ย่ำเท้าที่ข้างเตียงได้'
-                break
-            }
-            case 6: {
-                str = 'ระดับ 7 สามารถเดินได้โดยมีผู้ช่วยเหลือ 2 คนหรือมากกว่า'
-                break
-            }
-            case 7: {
-                str = 'ระดับ 8 สามารถเดินได้โดยมีผู้ช่วยเหลือ 1 คน'
-                break
-            }
-            case 8: {
-                str = 'ระดับ 9 สามารถเดินได้เองโดยมีอุปกรณ์ช่วย'
-                break
-            }
-            case 9: {
-                str = 'ระดับ 10 สามารถเดินได้เอง'
-                break
-            }
-            case 10: {
-                str = 'สิ้นสุด'
-                break
-            }
-            default: str = ''
-        }
-        return (<View style={[styles.container, { alignSelf: 'stretch', padding: 20, paddingHorizontal: 50 }]}>
-            <View style={{ flexDirection: 'column' }}>
-                <ActivityProgress onLevelChanged={this.onLevelChanged} progress={this.state.progress} />
-                <Button
-                    raised
-                    title='ระดับก่อนหน้า'
-                    fontSize={22}
-                    onPress={this.levelDown} />
-            </View>
-            <View style={{ flexDirection: 'column' }}>
-                <Timer />
-                <Text style={{ fontSize: 24, alignSelf: 'center' }}>ถัดไป : {str}</Text>
-                <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                    <Button
-                        raised
-                        backgroundColor='#118929'
-                        title='ระดับถัดไป'
-                        fontSize={22}
-                        onPress={this.levelUp} />
-                    <Button
-                        raised
-                        backgroundColor='#f49842'
-                        title='สิ้นสุด'
-                        fontSize={22}
-                        onPress={this.finish} />
-                </View>
-            </View>
-        </View>
+        // let str = ''
+        // switch (this.state.level) {
+        //     case 0: {
+        //         str = 'ระดับ 1 นั่งบนเตียง ออกกำลังกายบนเตียง'
+        //         break
+        //     }
+        //     case 1: {
+        //         str = 'ระดับ 2 มีผู้ช่วยเหลือผู้ป่วยมานั่งบนเก้าอี้ (ไม่ยืน)'
+        //         break
+        //     }
+        //     case 2: {
+        //         str = 'ระดับ 3 นั่งที่ขอบเตียง'
+        //         break
+        //     }
+        //     case 3: {
+        //         str = 'ระดับ 4 ยืนที่ข้างเตียง'
+        //         break
+        //     }
+        //     case 4: {
+        //         str = 'ระดับ 5 สามารถย้ายจากเตียงไปนั่งที่เก้าอี้ได้'
+        //         break
+        //     }
+        //     case 5: {
+        //         str = 'ระดับ 6 ก้าวขา ย่ำเท้าที่ข้างเตียงได้'
+        //         break
+        //     }
+        //     case 6: {
+        //         str = 'ระดับ 7 สามารถเดินได้โดยมีผู้ช่วยเหลือ 2 คนหรือมากกว่า'
+        //         break
+        //     }
+        //     case 7: {
+        //         str = 'ระดับ 8 สามารถเดินได้โดยมีผู้ช่วยเหลือ 1 คน'
+        //         break
+        //     }
+        //     case 8: {
+        //         str = 'ระดับ 9 สามารถเดินได้เองโดยมีอุปกรณ์ช่วย'
+        //         break
+        //     }
+        //     case 9: {
+        //         str = 'ระดับ 10 สามารถเดินได้เอง'
+        //         break
+        //     }
+        //     case 10: {
+        //         str = 'สิ้นสุด'
+        //         break
+        //     }
+        //     default: str = ''
+        // }
+        return (
+            // <View style={[styles.container, { alignSelf: 'stretch', padding: 20, paddingHorizontal: 50 }]}>
+            //     <View style={{ flexDirection: 'column' }}>
+            //         <ActivityProgress onLevelChanged={this.onLevelChanged} progress={this.state.progress} />
+            //         <Button
+            //             raised
+            //             title='ระดับก่อนหน้า'
+            //             fontSize={22}
+            //             onPress={this.levelDown} />
+            //     </View>
+            //     <View style={{ flexDirection: 'column' }}>
+            //         <Timer />
+            //         <Text style={{ fontSize: 24, alignSelf: 'center' }}>ถัดไป : {str}</Text>
+            //         <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+            //             <Button
+            //                 raised
+            //                 backgroundColor='#118929'
+            //                 title='ระดับถัดไป'
+            //                 fontSize={22}
+            //                 onPress={this.levelUp} />
+            //             <Button
+            //                 raised
+            //                 backgroundColor='#f49842'
+            //                 title='สิ้นสุด'
+            //                 fontSize={22}
+            //                 onPress={this.finish} />
+            //         </View>
+            //     </View>
+            // </View>
+            <DoingActivity />
         )
     }
 
     renderBorgScale = () => {
-        return (<View style={[styles.container, {flexDirection: 'column'}]}>
+        return (<View style={[styles.container, { flexDirection: 'column' }]}>
             <Text style={{ fontSize: 24, alignSelf: 'center' }}>รู้สึกเหนื่อยไหม?</Text>
             <Borg></Borg>
         </View>
