67ba0a68c2c9f8bc86960ac6a0a6c26236bbfcca
zigext <zigext@gmail.com>
2018-01-02 14:28:02 +0700

    Borg scale slider ***BUG when backward to this step and change borg value, the picture will not change

--- HPHRp1/app/components/Step1.activity.post.js
+++ HPHRp1/app/components/Step1.activity.post.js
@@ -1,71 +1,123 @@
 import React from 'react'
-import { View, Text, StyleSheet, TouchableHighlight, Image } from 'react-native'
-import { Button } from 'react-native-elements'
-import Icon from 'react-native-vector-icons/FontAwesome'
+import { View, Text, StyleSheet, TouchableHighlight, Image, ScrollView } from 'react-native'
+import { Icon, Slider } from 'react-native-elements'
+import common from '../styles/common'
 // import styles from '../styles/index'
 
 
 export default class Step1Post extends React.Component {
     constructor(props) {
         super(props)
+        this.state = {
+            borg: 6,
+        }
     }
     onPress = () => {
 
     }
 
+    onForward = () => {
+        this.props.onDataChange('borg', this.state.borg)
+        this.props.onStepChange(this.props.step + 1)
+    }
+
+    renderImage = () => {
+        let value
+        (this.props.borg)? value = this.props.borg : value = this.state.borg
+        if (value >= 6 && value <= 8)
+            return <Image source={require('../../assets/images/borg_great.png')} style={styles.imageInButtonImage} />
+        else if (value === 9)
+            return <Image source={require('../../assets/images/borg_good.png')} style={styles.imageInButtonImage} />
+        else if (value >= 10 && value <= 11)
+            return <Image source={require('../../assets/images/borg_normal.png')} style={styles.imageInButtonImage} />
+        else if (value >= 12 && value <= 13)
+            return <Image source={require('../../assets/images/borg_tired.png')} style={styles.imageInButtonImage} />
+        else
+            return <Image source={require('../../assets/images/borg_bad.png')} style={styles.imageInButtonImage} />
+    }
+
     render() {
+        // this.props.borg? this.state.borg = this.props.borg : this.state.borg = 6
         return (
-            <View style={styles.container}>
-                <Text style={styles.text}>โปรดเลือกระดับความเหนื่อย</Text>
+            <View style={{ flex: 1, flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'stretch', marginVertical: 15, marginHorizontal: 20 }}>
+                <ScrollView style={{ flex: 1 }}>
+                    <Text style={styles.text}>โปรดเลือกระดับความเหนื่อย</Text>
+                    {this.renderImage()}
+                    <View style={{ marginHorizontal: 300 }}>
+                        <Slider
+                            value={this.props.borg? this.props.borg: this.state.borg}
+                            onValueChange={(borg) => this.setState({ borg })}
+                            minimumValue={6}
+                            maximumValue={20}
+                            step={1}
+                            thumbTintColor={common.primaryColor}
+                            minimumTrackTintColor={common.primaryColor} />
+                    </View>
+                    <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+                        <Text style={{ fontSize: 18, marginHorizontal: 300 }}>Borg scale: {this.props.borg? this.props.borg: this.state.borg}</Text>
+                        <Icon
+                            raised
+                            reverse
+                            name='ios-arrow-forward'
+                            type='ionicon'
+                            color={common.accentColor}
+                            size={35}
+                            onPress={this.onForward}
+                        />
+                    </View>
 
-                <TouchableHighlight
-                    style={styles.button}
-                    onPress={this.onPress}
-                    underlayColor='#99d9f4'
-                >
+                    {/*<View style={styles.container}>
+                    
+                    <TouchableHighlight
+                        style={styles.button}
+                        onPress={this.onPress}
+                        underlayColor='#99d9f4'
+                    >
+                        <View>
+                            <Image source={require('../../assets/images/borg_great.png')} style={styles.imageInButtonImage}></Image>
+                        </View>
+                    </TouchableHighlight>
+                    <TouchableHighlight
+                        style={styles.button}
+                        onPress={this.onPress}
+                        underlayColor='#99d9f4'
+                    >
+                        <View>
+                            <Image source={require('../../assets/images/borg_good.png')} style={styles.imageInButtonImage}></Image>
+                        </View>
+                    </TouchableHighlight>
+                    <TouchableHighlight
+                        style={styles.button}
+                        onPress={this.onPress}
+                        underlayColor='#99d9f4'
+                    >
+                        <View>
+                            <Image source={require('../../assets/images/borg_normal.png')} style={styles.imageInButtonImage}></Image>
+                        </View>
+                    </TouchableHighlight>
+                    <TouchableHighlight
+                        style={styles.button}
+                        onPress={this.onPress}
+                        underlayColor='#99d9f4'
+                    >
+                        <View>
+                            <Image source={require('../../assets/images/borg_tired.png')} style={styles.imageInButtonImage}></Image>
+                        </View>
+                    </TouchableHighlight>
+                    <TouchableHighlight
+                        style={styles.button}
+                        onPress={this.onPress}
+                        underlayColor='#99d9f4'
+                    >
+                        <View>
+                            <Image source={require('../../assets/images/borg_bad.png')} style={styles.imageInButtonImage}></Image>
+                        </View>
+                    </TouchableHighlight>
                     <View>
-                        <Image source={require('../../assets/images/borg_great.png')} style={styles.imageInButtonImage}></Image>
-                    </View>
-                </TouchableHighlight>
-                <TouchableHighlight
-                    style={styles.button}
-                    onPress={this.onPress}
-                    underlayColor='#99d9f4'
-                >
-                    <View>
-                        <Image source={require('../../assets/images/borg_good.png')} style={styles.imageInButtonImage}></Image>
-                    </View>
-                </TouchableHighlight>
-                <TouchableHighlight
-                    style={styles.button}
-                    onPress={this.onPress}
-                    underlayColor='#99d9f4'
-                >
-                    <View>
-                        <Image source={require('../../assets/images/borg_normal.png')} style={styles.imageInButtonImage}></Image>
-                    </View>
-                </TouchableHighlight>
-                <TouchableHighlight
-                    style={styles.button}
-                    onPress={this.onPress}
-                    underlayColor='#99d9f4'
-                >
-                    <View>
-                        <Image source={require('../../assets/images/borg_tired.png')} style={styles.imageInButtonImage}></Image>
-                    </View>
-                </TouchableHighlight>
-                <TouchableHighlight
-                    style={styles.button}
-                    onPress={this.onPress}
-                    underlayColor='#99d9f4'
-                >
-                    <View>
-                        <Image source={require('../../assets/images/borg_bad.png')} style={styles.imageInButtonImage}></Image>
-                    </View>
-                </TouchableHighlight>
-                <View>
 
-                </View>
+                    </View>
+                </View>*/}
+                </ScrollView>
             </View>
         )
     }
@@ -82,7 +134,6 @@ const styles = StyleSheet.create({
 
     },
     button: {
-
         backgroundColor: '#FFFDF9',
         borderColor: common.primaryColor,
         borderWidth: 3,
@@ -91,22 +142,24 @@ const styles = StyleSheet.create({
         marginVertical: 10,
         paddingHorizontal: 10,
         alignSelf: 'stretch',
-        justifyContent: 'center'
+        justifyContent: 'center',
+        // shadowOffset: { width: 0, height: 5 },
+        // shadowOpacity: 0.8,
+        // shadowRadius: 3,
+        // shadowColor: '#000',
+        // elevation: 1,
     },
     imageInButtonImage: {
         resizeMode: 'contain',
-        margin: 10,
+        margin: 12,
         alignItems: 'center',
         height: 100,
         width: 100,
         alignSelf: 'center',
-        //      shadowOffset: { width: 0, height: 2 },
-        // shadowOpacity: 0.8,
-        // shadowRadius: 2,
-        // shadowColor: '#000',
-        // elevation: 1,
     },
     text: {
-        fontSize: 20
+        fontSize: 20,
+        color: common.grey,
+        textAlign: 'center',
     }
 })
