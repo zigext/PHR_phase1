5017d4649cfdc4fff14cacbad24158e6b914457e
zigext <zigext@gmail.com>
2018-01-22 21:28:08 +0700

    Move prepare data for x and y axis to Progress scene. Wait for callback before set state to 'searching'

---
+++ HPHRp1/app/containers/InitialProgress.js
@@ -0,0 +1,190 @@
+import React from 'react'
+import { AppRegistry, StyleSheet, Text, View, processColor, TouchableHighlight, ToastAndroid, Picker } from 'react-native'
+import { connect } from 'react-redux'
+import { BarChart } from 'react-native-charts-wrapper'
+import { Icon, Button } from 'react-native-elements'
+import _ from 'lodash'
+import moment from 'moment'
+import DatePicker from 'react-native-datepicker'
+import RecentLevelChart from '../components/RecentLevelChart'
+import common from '../styles/common'
+
+export default class InitialProgress extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            recentActivity: this.props.recentActivity,
+        }
+    }
+
+    formatDate = (date, format) => {
+        return moment(date).format(format)
+    }
+
+    formatTime = (time, format) => {
+        return moment(time).format(format)
+    }
+
+    startSearching = async () => {
+            await this.props.searching((wrongDate) => {
+                if(wrongDate === true) {
+                    return
+                }
+                else {
+                    this.props.startSearching()
+                }
+            })
+            
+    }
+
+    onSelect = type => {
+        this.props.handleSearchChange('type', type)
+    }
+
+    renderLastActivity = () => {
+
+        if (this.props.recentActivity) {
+            return (
+                <View style={styles.recentActivityContainer}>
+                    <Text style={[styles.text, { fontWeight: 'bold', textAlign: 'left' }]}>กิจกรรมครั้งล่าสุด</Text>
+                    <Text style={styles.text}>{this.formatDate(this.props.recentActivity.date, 'DD MMM')} เวลา {this.formatTime(this.props.recentActivity.results.timeStart, 'HH:mm')}  ขั้นที่ {this.props.recentActivity.results.result.maxLevel}  
+                        {this.props.recentActivity.results.result.levelTitle}
+                    </Text>
+                </View>
+            )
+        }
+
+        else {
+            return <Text style={styles.text}>ยังไม่เริ่มทำกิจกรรมฟื้นฟูหัวใจ</Text>
+        }
+    }
+
+    render() {
+        return (
+            <View style={{ flex: 1 }}>
+                <View style={{ flex: 1, backgroundColor: 'green', flexDirection: 'row' }}>
+
+                    <View style={{ flex: 1, backgroundColor: 'pink' }}>
+                        <RecentLevelChart dataRecentActivities={this.props.dataRecentActivities} xAxisRecentActivities={this.props.xAxisRecentActivities} yAxisRecentActivities={this.props.yAxisRecentActivities} markerRecentActivities={this.props.markerRecentActivities} legendRecentActivities={this.props.legendRecentActivities} />
+                    </View>
+                    <View style={{ flex: 1, backgroundColor: 'yellow', flexDirection: 'column' }}>
+                        <View style={{ flex: 0.8, backgroundColor: 'orange', padding: 20 }}>
+                            <Text style={styles.formText}>เลือกผลการทำกิจกรรม</Text>
+                            <View style={styles.buttonDown}>
+                                <Picker selectedValue={this.props.type} onValueChange={(type, idx) => this.onSelect(type)}>
+                                    <Picker.Item label='ขั้นที่ทำได้' value='maxLevel' />
+                                    <Picker.Item label='ระยะเวลาที่ใช้' value='duration' />
+                                </Picker>
+                            </View>
+
+                            <View style={{ flexDirection: 'row', flex: 1 }}>
+                                <View style={{ flex: 1 }}>
+                                    <Text style={styles.formText}>ตั้งแต่วันที่</Text>
+                                    <DatePicker
+                                        mode='date'
+                                        date={this.props.startDate}
+                                        format='DD/MM/YYYY'
+                                        minDate={this.props.admitDate ? this.props.admitDate : this.props.startDate}
+                                        maxDate={new Date()}
+                                        style={styles.elementContainer}
+                                        customStyles={styles.customStyles}
+                                        onDateChange={startDate => this.props.handleSearchChange('startDate', startDate)}
+                                    />
+                                </View>
+                                <View style={{ flex: 1 }}>
+                                    <Text style={styles.formText}>ถึงวันที่</Text>
+                                    <DatePicker
+                                        mode='date'
+                                        date={this.props.endDate}
+                                        format='DD/MM/YYYY'
+                                        minDate={this.props.startDate ? this.props.startDate : this.props.endDate}
+                                        maxDate={new Date()}
+                                        style={styles.elementContainer}
+                                        customStyles={styles.customStyles}
+                                        onDateChange={endDate => this.props.handleSearchChange('endDate', endDate)}
+                                    />
+                                </View>
+                            </View>
+
+                            <Icon
+                                raised
+                                reverse
+                                name='search'
+                                type='feather'
+                                color={common.primaryColorDark}
+                                size={25}
+                                containerStyle={{ alignSelf: 'flex-end' }}
+                                onPress={this.startSearching}
+                            />
+
+                        </View>
+                        <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+                            {this.renderLastActivity()}
+                        </View>
+
+
+                    </View>
+                </View>
+            </View>
+        )
+    }
+}
+
+const styles = {
+    container: {
+        flex: 1,
+        backgroundColor: '#F5FCFF'
+    },
+    levelContainer: {
+        borderWidth: 10,
+        width: 100,
+        height: 100,
+        borderRadius: 100 / 2,
+        borderColor: common.primaryColor
+    },
+    recentActivityContainer: {
+        borderWidth: 3,
+        borderRadius: 10,
+        borderColor: common.primaryColor,
+
+    },
+    text: {
+        fontSize: 20,
+        textAlign: 'center',
+        color: common.grey,
+        margin: 10
+    },
+    formText: {
+        fontSize: 17,
+        color: common.grey,
+        margin: 10,
+        fontWeight: 'bold'
+    },
+    buttonDown: {
+        flexDirection: 'column',
+        marginBottom: 10,
+        paddingLeft: 20,
+        width: '80%',
+        alignSelf: 'center',
+        borderRadius: 10,
+        borderWidth: 1,
+        borderColor: common.grey
+    },
+    elementContainer: {
+        width: '80%',
+        marginBottom: 10,
+        borderColor: common.grey
+    },
+    customStyles: {
+        dateIcon: {
+            position: 'absolute',
+            left: 0,
+            top: 4,
+            marginLeft: 5
+        },
+        dateText: {
+            marginLeft: 30
+        }
+    }
+}
---
+++ HPHRp1/app/containers/SearchProgress.js
@@ -0,0 +1,193 @@
+import React from 'react'
+import { AppRegistry, StyleSheet, Text, View, processColor, ScrollView, Picker } from 'react-native'
+import { connect } from 'react-redux'
+import { BarChart, LineChart } from 'react-native-charts-wrapper'
+import { Icon } from 'react-native-elements'
+import DatePicker from 'react-native-datepicker'
+import { last } from 'lodash'
+import moment from 'moment'
+import _ from 'lodash'
+import Chart from '../components/Chart'
+import common from '../styles/common'
+
+let formatDate = (format, date) => {
+    return moment(date).format(format)
+}
+
+export default class SearchProgress extends React.Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+
+        }
+    }
+
+    onSelect = type => {
+        this.props.handleSearchChange('type', type)
+    }
+
+    searchProgress = async () => {
+        await this.props.searching((wrongDate) => {
+            if (wrongDate === true) {
+                return
+            }
+            else {
+               
+            }
+        })
+    }
+
+    reset = () => {
+        this.props.resetState()
+    }
+
+    render() {
+        return (
+            <View style={{ flex: 1 }}>
+
+                <View style={{ margin: 20, flex: 1 }}>
+                    <ScrollView contentContainerStyle={{ flex: 1 }}>
+
+                        <View style={styles.formContainer}>
+
+                            <View style={{ flexDirection: 'row', flex: 1 }}>
+                                <View style={{ flex: 1 }}>
+                                    <Text style={styles.formText}>เลือกผลการทำกิจกรรม</Text>
+                                    <View style={styles.buttonDown}>
+                                        <Picker selectedValue={this.props.type} onValueChange={(type, idx) => this.onSelect(type)}>
+                                            <Picker.Item label='ขั้นที่ทำได้' value='maxLevel' />
+                                            <Picker.Item label='ระยะเวลาที่ใช้' value='duration' />
+                                        </Picker>
+                                    </View>
+                                </View>
+
+                                <View style={{ flex: 1 }}>
+                                    <Text style={styles.formText}>ตั้งแต่วันที่</Text>
+                                    <DatePicker
+                                        mode='date'
+                                        date={this.props.startDate}
+                                        format='DD/MM/YYYY'
+                                        minDate={this.props.admitDate ? this.props.admitDate : this.props.startDate}
+                                        maxDate={new Date()}
+                                        style={styles.elementContainer}
+                                        customStyles={styles.customStyles}
+                                        onDateChange={startDate => this.props.handleSearchChange('startDate', startDate)}
+                                    />
+                                </View>
+                                <View style={{ flex: 1 }}>
+                                    <Text style={styles.formText}>ถึงวันที่</Text>
+                                    <DatePicker
+                                        mode='date'
+                                        date={this.props.endDate}
+                                        format='DD/MM/YYYY'
+                                        minDate={this.props.startDate ? this.props.startDate : this.props.endDate}
+                                        maxDate={new Date()}
+                                        style={styles.elementContainer}
+                                        customStyles={styles.customStyles}
+                                        onDateChange={endDate => this.props.handleSearchChange('endDate', endDate)}
+                                    />
+                                </View>
+                                <Icon
+                                    raised
+                                    reverse
+                                    name='search'
+                                    type='feather'
+                                    color={common.primaryColorDark}
+                                    size={25}
+                                    containerStyle={{ alignSelf: 'flex-end' }}
+                                    onPress={this.searchProgress}
+                                />
+                                <Icon
+                                    raised
+                                    reverse
+                                    name='settings-backup-restore'
+                                    color='#d6d4e0'
+                                    size={25}
+                                    containerStyle={{ alignSelf: 'flex-end' }}
+                                    onPress={this.reset}
+                                />
+                            </View>
+
+
+                        </View>
+
+                        <View style={styles.chartContainer}>
+                            <Chart dataActivities={this.props.dataActivities} xAxisActivities={this.props.xAxisActivities} yAxisActivities={this.props.yAxisActivities} markerActivities={this.props.markerActivities} legendActivities={this.props.legendActivities} activityResult={this.props.activityResult} type={this.props.type} />
+                        </View>
+                    </ScrollView>
+
+                </View>
+            </View>
+        )
+    }
+}
+
+const styles = {
+    container: {
+        flex: 1,
+        backgroundColor: '#F5FCFF'
+    },
+    chart: {
+        flex: 1
+    },
+    levelContainer: {
+        borderWidth: 10,
+        width: 100,
+        height: 100,
+        borderRadius: 100 / 2,
+        borderColor: common.primaryColor
+    },
+    formContainer: {
+        flex: 0.4,
+        backgroundColor: 'yellow'
+    },
+    chartContainer: {
+        flex: 1,
+        backgroundColor: 'pink'
+    },
+    buttonDown: {
+        flexDirection: 'column',
+        marginBottom: 10,
+        paddingLeft: 20,
+        width: '80%',
+        alignSelf: 'center',
+        borderRadius: 10,
+        borderWidth: 1,
+        borderColor: common.grey
+    },
+    recentActivityContainer: {
+        borderWidth: 3,
+        borderRadius: 10,
+        borderColor: common.primaryColor
+    },
+    text: {
+        fontSize: 17,
+        textAlign: 'center',
+        color: common.grey,
+        margin: 10
+    },
+    formText: {
+        fontSize: 17,
+        color: common.grey,
+        margin: 10,
+        fontWeight: 'bold'
+    },
+    elementContainer: {
+        width: '80%',
+        marginBottom: 10,
+        borderColor: common.grey
+    },
+    customStyles: {
+        dateIcon: {
+            position: 'absolute',
+            left: 0,
+            top: 4,
+            marginLeft: 5
+        },
+        dateText: {
+            marginLeft: 30
+        }
+    }
+}
