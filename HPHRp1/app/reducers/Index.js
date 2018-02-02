d7ef323b476d077daae6e37050823fba25b472d0
zigext <zigext@gmail.com>
2018-02-02 17:27:03 +0700

    Change combineReducers and use rootReducer to reset states when log out

--- HPHRp1/app/reducers/Index.js
+++ HPHRp1/app/reducers/Index.js
@@ -1,4 +1,5 @@
-import * as userReducer from './UserReducer'
+// import * as userReducer from './UserReducer'
+import userReducer from './UserReducer'
 import * as activityReducer from './ActivityReducer'
 import { combineReducers } from 'redux'
 
@@ -16,9 +17,21 @@ import { combineReducers } from 'redux'
 // console.log("root reducer = ", rootReducer)
 // export default rootReducer
 
-export default combineReducers(Object.assign(
-    
-    userReducer,
-    
-    
-))
+// export default combineReducers(Object.assign(
+//     userReducer,
+// ))
+
+const appReducer = combineReducers({
+    userReducer
+    }
+)
+
+const rootReducer = (state, action) => {
+  if (action.type === 'LOG_OUT') {
+    state = undefined
+  }
+
+  return appReducer(state, action)
+}
+
+export default rootReducer
