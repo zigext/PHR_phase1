ec597d1f0577f7673fd95be2c7d294d5a2429bde
zigext <zigext@gmail.com>
2018-02-06 18:23:55 +0700

    Edit styles

--- HPHRp1/app/components/Instructions.js
+++ HPHRp1/app/components/Instructions.js
@@ -23,7 +23,8 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'center',
-        alignItems: 'center'
+        alignItems: 'center',
+        marginTop: 20
     },
     text: {
         fontSize: 22,
