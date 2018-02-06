1ad4135e3be8936b6c284aba2e3bef63256e0a39
zigext <zigext@gmail.com>
2018-02-06 18:27:04 +0700

    Change from show only 1 borg face and change it according to slider's value. To show 5 borg faces and change their opacity according to slider's value

--- HPHRp1/app/components/Step1.activity.post.js
+++ HPHRp1/app/components/Step1.activity.post.js
@@ -21,19 +21,40 @@ export default class Step1Post extends React.Component {
         this.props.onStepChange(this.props.step + 1)
     }
 
+    setBorgAndOpacity = (borg) => {
+        this.setState({
+            borg
+        })
+    }
+
+    // renderDescription = () => {
+    //     let value
+    //     (this.props.borg) ? value = this.props.borg : value = this.state.borg
+    //     if (value >= 6 && value <= 8)
+    //         return <Text style={styles.descriptionText}>เหมือนนั่งเล่นสบายๆ                                              </Text>
+    //     else if (value === 9)
+    //         return <Text style={styles.descriptionText}>เหนื่อยเล็กน้อย พูดคุยได้ปกติ                              </Text>
+    //     else if (value >= 10 && value <= 11)
+    //         return <Text style={styles.descriptionText}>เหนื่อยมากขึ้นแต่ทนได้ พูดสื่อสารได้ ใจไม่สั่น</Text>
+    //     else if (value >= 12 && value <= 13)
+    //         return <Text style={styles.descriptionText}>เหนื่อย หายใจเร็ว พูดได้เป็นคำๆ ต้องหยุดพัก  </Text>
+    //     else
+    //         return <Text style={styles.descriptionText}>เหนื่อยจนหอบ พูดไม่ไหว ใจสั่น                           </Text>
+    // }
+
     renderDescription = () => {
         let value
         (this.props.borg) ? value = this.props.borg : value = this.state.borg
         if (value >= 6 && value <= 8)
-            return <Text style={styles.descriptionText}>เหมือนนั่งเล่นสบายๆ                                              </Text>
+            return <Text style={styles.descriptionText}>เหมือนนั่งเล่นสบายๆ</Text>
         else if (value === 9)
-            return <Text style={styles.descriptionText}>เหนื่อยเล็กน้อย พูดคุยได้ปกติ                              </Text>
+            return <Text style={styles.descriptionText}>เหนื่อยเล็กน้อย พูดคุยได้ปกติ</Text>
         else if (value >= 10 && value <= 11)
             return <Text style={styles.descriptionText}>เหนื่อยมากขึ้นแต่ทนได้ พูดสื่อสารได้ ใจไม่สั่น</Text>
         else if (value >= 12 && value <= 13)
-            return <Text style={styles.descriptionText}>เหนื่อย หายใจเร็ว พูดได้เป็นคำๆ ต้องหยุดพัก  </Text>
+            return <Text style={styles.descriptionText}>เหนื่อย หายใจเร็ว พูดได้เป็นคำๆ ต้องหยุดพัก</Text>
         else
-            return <Text style={styles.descriptionText}>เหนื่อยจนหอบ พูดไม่ไหว ใจสั่น                           </Text>
+            return <Text style={styles.descriptionText}>เหนื่อยจนหอบ พูดไม่ไหว ใจสั่น</Text>
     }
 
     renderImage = () => {
@@ -51,97 +72,122 @@ export default class Step1Post extends React.Component {
             return <Image source={require('../../assets/images/borg_bad.png')} style={styles.image} />
     }
 
-    render() {
-        // this.props.borg? this.state.borg = this.props.borg : this.state.borg = 6
+    renderImageWithOpacity = () => {
+        let value
+        (this.props.borg) ? value = this.props.borg : value = this.state.borg
+        if (value >= 6 && value <= 8)
+            return (
+                <View style={styles.imageContainer}>
+                    <Image source={require('../../assets/images/borg_great.png')} style={styles.image} />
+                    <Image source={require('../../assets/images/borg_good.png')} style={[styles.image, { opacity: 0.3 }]} />
+                    <Image source={require('../../assets/images/borg_normal.png')} style={[styles.image, { opacity: 0.3 }]} />
+                    <Image source={require('../../assets/images/borg_tired.png')} style={[styles.image, { opacity: 0.3 }]} />
+                    <Image source={require('../../assets/images/borg_bad.png')} style={[styles.image, { opacity: 0.3 }]} />
+                </View>
+            )
+        else if (value === 9)
+            return (
+                <View style={styles.imageContainer}>
+                    <Image source={require('../../assets/images/borg_great.png')} style={[styles.image, { opacity: 0.3 }]} />
+                    <Image source={require('../../assets/images/borg_good.png')} style={styles.image} />
+                    <Image source={require('../../assets/images/borg_normal.png')} style={[styles.image, { opacity: 0.3 }]} />
+                    <Image source={require('../../assets/images/borg_tired.png')} style={[styles.image, { opacity: 0.3 }]} />
+                    <Image source={require('../../assets/images/borg_bad.png')} style={[styles.image, { opacity: 0.3 }]} />
+                </View>
+            )
+        else if (value >= 10 && value <= 11)
+            return (
+                <View style={styles.imageContainer}>
+                    <Image source={require('../../assets/images/borg_great.png')} style={[styles.image, { opacity: 0.3 }]} />
+                    <Image source={require('../../assets/images/borg_good.png')} style={[styles.image, { opacity: 0.3 }]} />
+                    <Image source={require('../../assets/images/borg_normal.png')} style={styles.image} />
+                    <Image source={require('../../assets/images/borg_tired.png')} style={[styles.image, { opacity: 0.3 }]} />
+                    <Image source={require('../../assets/images/borg_bad.png')} style={[styles.image, { opacity: 0.3 }]} />
+                </View>
+            )
+        else if (value >= 12 && value <= 13)
+            return (
+                <View style={styles.imageContainer}>
+                    <Image source={require('../../assets/images/borg_great.png')} style={[styles.image, { opacity: 0.3 }]} />
+                    <Image source={require('../../assets/images/borg_good.png')} style={[styles.image, { opacity: 0.3 }]} />
+                    <Image source={require('../../assets/images/borg_normal.png')} style={[styles.image, { opacity: 0.3 }]} />
+                    <Image source={require('../../assets/images/borg_tired.png')} style={styles.image} />
+                    <Image source={require('../../assets/images/borg_bad.png')} style={[styles.image, { opacity: 0.3 }]} />
+                </View>
+            )
         return (
-            // <View style={{ flex: 1, flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'stretch', marginVertical: 15, marginHorizontal: 20 }}>
-            //     <ScrollView style={{ flex: 1 }}>
-            //         <Text style={styles.text}>โปรดเลือกระดับความเหนื่อย</Text>
-            //         {this.renderImage()}
-            //         <View style={{ marginHorizontal: 300 }}>
-            //             <Slider
-            //                 value={this.props.borg ? this.props.borg : this.state.borg}
-            //                 onValueChange={(borg) => this.setState({ borg })}
-            //                 minimumValue={6}
-            //                 maximumValue={20}
-            //                 step={1}
-            //                 thumbTintColor={common.primaryColor}
-            //                 minimumTrackTintColor={common.primaryColor} />
-            //         </View>
-            //         <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-            //             <Text style={{ fontSize: 18, marginHorizontal: 300 }}>Borg scale: {this.props.borg ? this.props.borg : this.state.borg}</Text>
-            //             <Icon
-            //                 raised
-            //                 reverse
-            //                 name='ios-arrow-forward'
-            //                 type='ionicon'
-            //                 color={common.accentColor}
-            //                 size={35}
-            //                 onPress={this.onForward}
-            //             />
-            //         </View>
-            //     </ScrollView>
-            // </View>
+            <View style={styles.imageContainer}>
+                <Image source={require('../../assets/images/borg_great.png')} style={[styles.image, { opacity: 0.3 }]} />
+                <Image source={require('../../assets/images/borg_good.png')} style={[styles.image, { opacity: 0.3 }]} />
+                <Image source={require('../../assets/images/borg_normal.png')} style={[styles.image, { opacity: 0.3 }]} />
+                <Image source={require('../../assets/images/borg_tired.png')} style={[styles.image, { opacity: 0.3 }]} />
+                <Image source={require('../../assets/images/borg_bad.png')} style={styles.image} />
+            </View>
+        )
+    }
 
-            <View style={{ flex: 1, flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'stretch', marginVertical: 15, marginHorizontal: 20 }}>
+    render() {
+        //Show 5 borg faces and change their opacity
+        return (
+            <View style={styles.container}>
                 <ScrollView style={{ flex: 1 }}>
-                    <Text style={[styles.text, {fontWeight: 'bold'}]}>โปรดเลือกระดับความเหนื่อย</Text>
-
-                    <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'center' }}>
-                        <View style={{ marginRight: 120, flex: 1 }}>
-                            {this.renderImage()}
-                            <View style={{}}>
-                                <Slider
-                                    value={this.props.borg ? this.props.borg : this.state.borg}
-                                    onValueChange={(borg) => this.setState({ borg })}
-                                    minimumValue={6}
-                                    maximumValue={20}
-                                    step={1}
-                                    thumbTintColor={common.primaryColor}
-                                    minimumTrackTintColor={common.primaryColor} />
-                            </View>
-                            <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-                                <Text style={{ fontSize: 19 }}>Borg scale: {this.props.borg ? this.props.borg : this.state.borg}</Text>
-                            </View>
-                        </View>
-
-                        <View style={{ justifyContent: 'center', alignItems: 'center', marginLeft: 120 }}>
-                            {this.renderDescription()}
-                            <Icon
-                                raised
-                                reverse
-                                name='ios-arrow-forward'
-                                type='ionicon'
-                                color={common.accentColor}
-                                size={35}
-                                onPress={this.onForward}
-                                containerStyle={{ alignSelf: 'flex-end', marginTop: 30 }}
-                            />
-                        </View>
-
+                    <Text style={[styles.text, { fontWeight: 'bold' }]}>โปรดเลือกระดับความเหนื่อย</Text>
+                    {this.renderImageWithOpacity()}
+                    <View style={{ marginHorizontal: 300 }}>
+                        <Slider
+                            value={this.props.borg ? this.props.borg : this.state.borg}
+                            onValueChange={(borg) => this.setState({ borg })}
+                            minimumValue={6}
+                            maximumValue={20}
+                            step={1}
+                            thumbTintColor={common.primaryColor}
+                            minimumTrackTintColor={common.primaryColor} />
                     </View>
 
+                    <Text style={[ styles.sliderText, {marginHorizontal: 300 }]}>Borg scale: {this.props.borg ? this.props.borg : this.state.borg}</Text>
+                    {/*<View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>*/}
+                    <View style={{ alignItems: 'center', marginTop: 16 }} >
+                        {this.renderDescription()}
+                    </View>
+                    {/*</View>*/}
+                    <Icon
+                        raised
+                        reverse
+                        name='ios-arrow-forward'
+                        type='ionicon'
+                        color={common.accentColor}
+                        size={35}
+                        onPress={this.onForward}
+                        containerStyle={{alignSelf: 'flex-end'}}
+                    />
                 </ScrollView>
             </View>
 
-            // <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'flex-start', alignItems: 'center', marginVertical: 15, marginHorizontal: 30 }}>
-            //     <ScrollView>
-            //         <Text style={styles.text}>โปรดเลือกระดับความเหนื่อย</Text>
-            //         <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'space-between' }}>
-            //             {this.renderImage()}
-            //             {this.renderDescription()}
-            //         </View>
-            //         <View style={{ flex: 0.5, flexDirection: 'column', justifyContent: 'center' }}>
-            //             <Slider
-            //                 value={this.props.borg ? this.props.borg : this.state.borg}
-            //                 onValueChange={(borg) => this.setState({ borg })}
-            //                 minimumValue={6}
-            //                 maximumValue={20}
-            //                 step={1}
-            //                 thumbTintColor={common.primaryColor}
-            //                 minimumTrackTintColor={common.primaryColor} />
-            //             <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-            //                 <Text style={{ fontSize: 18 }}>Borg scale: {this.props.borg ? this.props.borg : this.state.borg}</Text>
+            //Display only 1 borg face and will change depending on slider's value
+            // <View style={styles.container}>
+            //     <ScrollView style={{ flex: 1 }}>
+            //         <Text style={[styles.text, {fontWeight: 'bold'}]}>โปรดเลือกระดับความเหนื่อย</Text>
+
+            //         <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'center' }}>
+            //             <View style={{ marginRight: 120, flex: 1 }}>
+            //                 {this.renderImage()}
+            //                 <View style={{}}>
+            //                     <Slider
+            //                         value={this.props.borg ? this.props.borg : this.state.borg}
+            //                         onValueChange={(borg) => this.setState({ borg })}
+            //                         minimumValue={6}
+            //                         maximumValue={20}
+            //                         step={1}
+            //                         thumbTintColor={common.primaryColor}
+            //                         minimumTrackTintColor={common.primaryColor} />
+            //                 </View>
+            //                 <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+            //                     <Text style={styles.sliderText}>Borg scale: {this.props.borg ? this.props.borg : this.state.borg}</Text>
+            //                 </View>
+            //             </View>
+
+            //             <View style={{ justifyContent: 'center', alignItems: 'center', marginLeft: 120 }}>
+            //                 {this.renderDescription()}
             //                 <Icon
             //                     raised
             //                     reverse
@@ -150,9 +196,12 @@ export default class Step1Post extends React.Component {
             //                     color={common.accentColor}
             //                     size={35}
             //                     onPress={this.onForward}
+            //                     containerStyle={{ alignSelf: 'flex-end', marginTop: 30 }}
             //                 />
             //             </View>
+
             //         </View>
+
             //     </ScrollView>
             // </View>
         )
@@ -163,35 +212,16 @@ export default class Step1Post extends React.Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'flex-start',
+        alignItems: 'stretch',
+        marginVertical: 15,
+        marginHorizontal: 20
+    },
+    imageContainer: {
+        flex: 1,
         flexDirection: 'row',
         justifyContent: 'center',
-        alignSelf: 'stretch',
-        padding: 20,
-
-    },
-    button: {
-        backgroundColor: '#FFFDF9',
-        borderColor: common.primaryColor,
-        borderWidth: 3,
-        borderRadius: 8,
-        marginHorizontal: 10,
-        marginVertical: 10,
-        paddingHorizontal: 10,
-        alignSelf: 'stretch',
-        justifyContent: 'center',
-        // shadowOffset: {width: 0, height: 5 },
-        // shadowOpacity: 0.8,
-        // shadowRadius: 3,
-        // shadowColor: '#000',
-        // elevation: 1,
-    },
-    imageInButtonImage: {
-        resizeMode: 'contain',
-        margin: 12,
-        alignItems: 'center',
-        height: 100,
-        width: 100,
-        alignSelf: 'center',
     },
     image: {
         resizeMode: 'contain',
@@ -205,10 +235,19 @@ const styles = StyleSheet.create({
         color: common.grey,
         textAlign: 'center',
     },
+    sliderText: {
+        fontSize: 19,
+        color: common.grey
+    },
+    // descriptionText: {
+    //     fontSize: 20,
+    //     color: common.grey,
+    //     textAlign: 'center',
+    //     marginTop: 30
+    // },
     descriptionText: {
         fontSize: 20,
         color: common.grey,
         textAlign: 'center',
-        marginTop: 30
     }
 })
