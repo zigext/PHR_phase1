c60907248bc618b43d192b9db842563588e7c22d
zigext <zigext@gmail.com>
2018-02-12 14:23:07 +0700

    User can delete surgery informayion

--- HPHRp1/app/components/SurgeryDetail.js
+++ HPHRp1/app/components/SurgeryDetail.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { StyleSheet, Text, View, ScrollView, Image, Alert } from 'react-native'
+import { StyleSheet, Text, View, ScrollView, Image, Alert, ToastAndroid } from 'react-native'
 import { Icon, Button } from 'react-native-elements'
 import { Actions } from 'react-native-router-flux'
 import { connect } from 'react-redux'
@@ -7,7 +7,7 @@ import Orientation from 'react-native-orientation'
 import styles from '../styles/index'
 import PropTypes from 'prop-types'
 import firebase from '../config/Firebase'
-import ApiUtils from '../components/ApiUtils'
+import ApiUtils from './ApiUtils'
 import { SERVER_IP, SURGERY } from '../config/Const'
 
 
@@ -41,47 +41,44 @@ export default class SurgeryDetail extends React.Component {
         )
 
     }
-    //Have not tested yet
-    deleteSurgery = () => {
-        // const path = `${SERVER_IP}${SURGERY}`
-        // await fetch(path, {
-        //     method: 'DELETE',
-        //     headers: {
-        //         'Accept': 'application/json',
-        //         'Content-Type': 'application/json',
-        //     },
-        //     body: JSON.stringify({
-        //         userid: '1416382941765846', //this.props.uid
-        //         hospitalid: this.props.surgery.hospital,
-        //         date: this.props.surgery.date,
-        //         time: this.props.surgery.time,
-        //     })
-        // })
-        //     .then(ApiUtils.checkStatus)
-        //     .then(responseData => {
-        //         console.log("Delete surgery success")
-        //         Actions.surgery()
+    
+    deleteSurgery = async () => {
+        const path = `${SERVER_IP}${SURGERY}?userid=${this.props.uid}&hospitalid=${this.props.surgery.hospital}&date=${this.props.surgery.date}&time=${this.props.surgery.time}`
+        await fetch(path, {
+            method: 'DELETE',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            }
+        })
+            .then(ApiUtils.checkStatus)
+            .then(response => response.json())
+            .then(responseData => {
+                console.log("Delete surgery success", responseData)
+                ToastAndroid.showWithGravity('ลบรายการสำเร็จ', ToastAndroid.SHORT, ToastAndroid.CENTER)
+                Actions.surgery()
 
-        //     })
-        //     .catch(error => {
-        //         console.log("Delete surgery failed = ", error)
-        //     })
+            })
+            .catch(error => {
+                console.log("Delete surgery failed = ", error)
+                ToastAndroid.showWithGravity('ผิดพลาด! ไม่สามารถลบรายการ', ToastAndroid.SHORT, ToastAndroid.CENTER)
+            })
     }
 
 
-render() {
-    return (
-        <View style={_styles.slide}>
-            <ScrollView>
-                <Text style={[styles.text, { fontWeight: 'bold', marginBottom: 25 }]}>วันที่ {this.props.surgery.date}  เวลา {this.props.surgery.time}  ประเภท {this.props.surgery.type}</Text>
-                <Text style={[styles.text, { marginBottom: 10 }]}>โรงพยาบาล: {this.props.surgery.hospital}</Text>
-                <Text style={[styles.text, { marginBottom: 10 }]}>แพทย์: {this.props.surgery.doctor}</Text>
-                <Text style={[styles.text, { marginBottom: 10 }]}>รายละเอียด: {this.props.surgery.note}</Text>
-                <Icon raised reverse name='delete' color='#f49842' onPress={this.deleteSurgeryPress} />
-            </ScrollView>
-        </View>
-    )
-}
+    render() {
+        return (
+            <View style={_styles.slide}>
+                <ScrollView>
+                    <Text style={[styles.text, { fontWeight: 'bold', marginBottom: 25 }]}>วันที่ {this.props.surgery.date}  เวลา {this.props.surgery.time}  ประเภท {this.props.surgery.type}</Text>
+                    <Text style={[styles.text, { marginBottom: 10 }]}>โรงพยาบาล: {this.props.surgery.hospital}</Text>
+                    <Text style={[styles.text, { marginBottom: 10 }]}>แพทย์: {this.props.surgery.doctor}</Text>
+                    <Text style={[styles.text, { marginBottom: 10 }]}>รายละเอียด: {this.props.surgery.note}</Text>
+                    <Icon raised reverse name='delete' color='#f49842' onPress={this.deleteSurgeryPress} />
+                </ScrollView>
+            </View>
+        )
+    }
 }
 
 const _styles = StyleSheet.create({
